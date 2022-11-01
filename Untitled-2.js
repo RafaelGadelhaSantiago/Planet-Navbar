c2af87a4d2e9bfd0fb60aaa0c4a1052846d093d4
Rafael <99349794+RafaelGadelhaSantiago@users.noreply.github.com>
2022-11-01 13:49:48 -0300

    Create Untitled-2.js

---
+++ Untitled-2.js
@@ -0,0 +1,69 @@
+// #nav
+function navBurgerMenu() {
+  const navCheck = document.querySelector(".navbar") !== null;
+  if (navCheck) {
+    const offCanvas = document.querySelector("[data-toggle='offcanvas']");
+    const collapse = document.querySelector(".navbar-collapse");
+    const overlay = document.querySelector(".overlay");
+    const navbar = document.querySelector(".navbar-nav");
+    const submenus = navbar.querySelectorAll(".sub-menu");
+
+    offCanvas.onclick = function () {
+      collapse.classList.toggle("open");
+      overlay.classList.toggle("overlayon");
+      offCanvas.classList.toggle("burgeron");
+      for (let submenu of submenus) {
+        submenu.classList.remove("open-sub");
+      }
+      for (const li of document.querySelectorAll("li.activeItem")) {
+        li.classList.remove("activeItem");
+      }
+    };
+
+    const nav = document.querySelector("nav");
+    const navHeight = nav.offsetHeight;
+    const transparencyOffset = 200;
+    let prevScrollpos = document.pageYOffset;
+    document.onscroll = function () {
+      /* Add dynamic transparency to nav */
+      if (nav.getAttribute("data-isTransparent")) {
+        if (
+          // document.body.scrollTop >= transparencyOffset || document.documentElement.scrollTop >= transparencyOffset
+          document.scrollingElement.scrollTop >= transparencyOffset ||
+          document.documentElement.scrollTop >= transparencyOffset
+        ) {
+          /*in some Chrome versions there is bug with 'document.body.scrollTop'*/
+          nav.setAttribute("data-isTransparent", "false");
+        } else {
+          nav.setAttribute("data-isTransparent", "true");
+        }
+      }
+    };
+  }
+
+  const links = document.querySelectorAll(".drop-down");
+
+  links.forEach((link) => {
+    const subMenu = link.querySelector(".sub-menu");
+
+    link.addEventListener("click", function () {
+      // if viewport is less than 992px
+      if (window.innerWidth < 992) {
+        if (this.classList.contains("activeItem")) {
+          this.classList.remove("activeItem");
+          subMenu.classList.remove("open-sub");
+        } else if (document.querySelector(".activeItem")) {
+          document.querySelector(".activeItem").classList.remove("activeItem");
+          this.classList.add("activeItem");
+          document.querySelector(".open-sub").classList.remove("open-sub");
+          subMenu.classList.add("open-sub");
+        } else {
+          this.classList.add("activeItem");
+          subMenu.classList.add("open-sub");
+        }
+      }
+    });
+  });
+}
+
+navBurgerMenu();
